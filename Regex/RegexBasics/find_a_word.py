0a21824f50f3a4baccb354dc68e0436627cbbc75
Patrick Dillon <patrickdillon26@gmail.com>
2015-11-13 20:50:50 +0000

    first to regex problems
    find_a_word not working

---
+++ Regex/RegexBasics/find_a_word.py
@@ -0,0 +1,23 @@
+import re
+
+def convert_string(s):
+     j=re.compile( "[^a-zA-Z0-9]*"+s+"([^a-zA-Z0-9_])+" )
+     return j
+def test_string(regex,sen):
+    return len(regex.findall(sen))
+
+    
+    
+
+
+num_sen=int(input())
+sentences=[]
+for i in range(num_sen):
+    sentences.append(input())
+
+
+num_tests=int(input())
+for i in range(num_tests):
+    reg=convert_string(input())
+    total=sum([test_string(reg,x) for x in sentences])
+    print(total)
